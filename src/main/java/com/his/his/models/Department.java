d7ac8d098269c77b2d7427f8d9fc3f21ef147cd8
KaranjitSaha <Karanjit.Saha@iiitb.ac.in>
2024-02-25 23:15:35 +0530

    Created Department model

---
+++ src/main/java/com/his/his/models/Department.java
@@ -0,0 +1,45 @@
+package com.his.his.models;
+
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@Table(name="department")
+public class Department {
+    public Department(){
+
+    }
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false )
+    private UUID departmentId;
+
+    @Column(updatable = false, nullable= false)
+    private String departmentName;
+
+    @Column(nullable = false)
+    private String departmentHead;
+
+    @Column(nullable = false)
+    private int noOfDoctors;
+
+    @Column(nullable = false)
+    private int noOfNurses;
+}
