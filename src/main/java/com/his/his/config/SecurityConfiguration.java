641821dcc5b0b587e27551ea67b874707b55ea6a
tejsharma15 <tejsharma815@gmail.com>
2024-05-05 17:01:08 +0530

    Gave getall department access to desk

--- src/main/java/com/his/his/config/SecurityConfiguration.java
+++ src/main/java/com/his/his/config/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration {
                         req.requestMatchers(WHITE_LIST_URL)
                                 .permitAll()
                                 .requestMatchers(GET,"/employee","/employee").hasAnyAuthority(ADMIN_READ.getPermission(),DESK_READ.getPermission(),HEAD_NURSE_READ.getPermission())
-                                .requestMatchers("/department","/department/{id}").hasAnyAuthority(DEP_UPDATE.getPermission())
+                                .requestMatchers("/department","/department/{id}").hasAnyAuthority(DEP_UPDATE.getPermission(),DESK_READ.getPermission())
                                 .requestMatchers(GET,"/patient","/patient/{id}","patient/transfer/{id}").hasAnyAuthority(DESK_READ.getPermission(),HEAD_NURSE_READ.getPermission())
                                 .requestMatchers("/patientDoctor").hasAnyAuthority(DESK_READ.getPermission())
                                 .requestMatchers(GET,"/employeeDepartment").hasAnyAuthority(NURSE_READ.getPermission(),HEAD_NURSE_READ.getPermission(),DOCTOR_READ.getPermission())
